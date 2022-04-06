b74b87f5b9e94de6b9abcbc127e2ddedb222b111
BeuBen <beuscart.ben@gmail.com>
2022-04-06 16:24:25 +0200

    Initial commit

---
+++ src/main/java/com/beuben/rpginvmanager/RpgInvManagerApplication.java
@@ -0,0 +1,13 @@
+package com.beuben.rpginvmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgInvManagerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RpgInvManagerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/beuben/rpginvmanager/RpgInvManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beuben.rpginvmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgInvManagerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
